c3a0ce61a519af55e073f9ab2ff0f93ad3a816da
DavidAkinwole <145507073+DavidAkinwole@users.noreply.github.com>
2026-03-31 18:13:08 +0100

    Added addition function for 3 numbers helper.js

--- helper.js
+++ helper.js
@@ -11,6 +11,10 @@ function sub(num1, num2){
   return num1 - num2
 }
 
-function multiplication(num1, num2) {
+function multiplication(num1, num2){
   return num1 * num2;
+}
+
+function addition(num1, num2, num3){
+  return num1 + num2 + num3;
 }
